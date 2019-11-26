fb0b1fefa7415d7e34c23edb52913f5f897acd28
RaiyKuo <xaiykou@gmail.com>
2019-11-26 08:16:54 -0600

    Upload Working

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -4,46 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.util.ArrayList;
 
-
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private MessageListAdapter adapter;
 
+    public class Message {                                                    // Message storage structure
+        String text, user;
+        public Message(String user, String text){                             // Constructor for new message
+            this.text = text;
+            this.user = user;
+        }
+    }
+
+    private ArrayList<Message> message = new ArrayList<>();                    // The message data list
+    public String message_out;                                                 // The message sent from cellphone
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //change start from here
 
-        recyclerView = findViewById(R.id.reyclerview_message_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final RecyclerView recyclerView = findViewById(R.id.message_list);      // RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));   // Set layout for RecyclerView
 
-        final ArrayList<String> data = new ArrayList<>();
-
-        Button mButton = (Button)findViewById(R.id.button_send);
-        final EditText mEdit  = (EditText)findViewById(R.id.edittext_chatbox);
+        Button mButton = (Button)findViewById(R.id.button_send);                // Send button
+        final EditText mEdit  = (EditText)findViewById(R.id.edittext_chatbox);  // input box for user to type message
 
         mButton.setOnClickListener(
                 new View.OnClickListener()
                 {
                     public void onClick(View view)
                     {
-                        String message_out = mEdit.getText().toString();
-                        data.add(message_out);
-                        mEdit.getText().clear();
-                        Log.v("EditText", message_out);
-                        adapter = new MessageListAdapter(this, data);
-                        recyclerView.setAdapter(adapter);
+                        message_out = mEdit.getText().toString();               // Read user input
+                        mEdit.getText().clear();                                // Clear input box after pressing "send" button
+
+                        ToServer.sendPost(message_out);                         // Send message to server
+                        message.add(new Message("me", message_out));       // Record sent message
+
+                        recyclerView.setAdapter(new MessageListAdapter(message)); // Display messages on the screen
                     }
                 });
-
     }
+
 }
